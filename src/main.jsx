07ff6984b2e515ddb0f7ddd7086a0b0e025f2816
sharonyapqs-cmd <sharonyap.qs@gmail.com>
2026-03-07 14:40:47 +0800

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BankReportingMVP from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BankReportingMVP />
+  </React.StrictMode>
+);
